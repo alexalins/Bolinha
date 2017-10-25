e0d78c54c897204930fd92e7706ad0a4cf5aa520
alexalins <alexa.glinsoo@gmail.com>
2017-10-25 20:38:38 -0300

    primeiro

---
+++ Assets/Materiais/MoveCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+	public GameObject objetoBolinha;
+	private Vector3  posicaoInicial;
+
+	void Start () {
+		
+		posicaoInicial = transform.position;
+		
+	}
+
+	void Update () {
+		transform.position = objetoBolinha.transform.position + posicaoInicial;
+	}
+}
---
+++ Assets/Materiais/MovimentoBolinha.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoBolinha : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float velocidade;
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody>();
+		
+	}
+	
+	void FixedUpdate () {
+		Vector3 move = new Vector3 (Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		rb.AddForce (move * velocidade);
+	}
+}
